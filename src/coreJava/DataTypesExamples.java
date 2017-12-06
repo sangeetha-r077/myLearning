a0f47c757cf63332a293fb6b789f3eddc7652251
Sangeetha Rajkumar <sangeetha.rajkumar@bangalore.accenture.bls.com>
2017-12-06 12:52:20 +0530

    commit

---
+++ src/coreJava/DataTypesExamples.java
@@ -0,0 +1,14 @@
+package coreJava;
+
+public class DataTypesExamples {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = a;
+        long c = 10000L;
+        byte d = (byte) c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(d);
+    }
+
+}
