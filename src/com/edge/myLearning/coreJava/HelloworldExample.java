52d9aba6bc0c70b0062ca3164f0a6a22a47a574d
Sangeetha <sangeetha.r.077@gmail.com>
2017-12-06 10:24:36 +0530

    My first commit

---
+++ src/com/edge/myLearning/coreJava/HelloworldExample.java
@@ -0,0 +1,7 @@
+package com.edge.myLearning.coreJava;
+
+public class HelloworldExample {
+    public static void main(String[] args) {
+        System.out.println("Hello World Java Program");
+    }
+}
